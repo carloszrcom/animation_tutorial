13c17b1cff9f67543b5a6976e6477b934b6fed8d
CarlosZR <carlos.z.r.78@gmail.com>
2021-06-18 13:35:03 +0200

    Primera forma de animar, básica y desordenada. Hardcoded animation.

---
+++ android/app/src/main/kotlin/com/carloszr/animation_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.carloszr.animation_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/basic_animation_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimationPage extends StatefulWidget {
+  AnimationPage({Key? key}) : super(key: key);
+
+  @override
+  _AnimationPageState createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    animController = AnimationController(
+      vsync: this, // The SingleTickerProviderStateMixin
+      duration: Duration(seconds: 5),
+    );
+
+    final curvedAnimation = CurvedAnimation(
+      parent: animController,
+      curve: Curves.bounceIn,
+      reverseCurve: Curves.easeOut,
+    );
+
+    animation = Tween<double>(
+      begin: 0,
+      end: 2 * math.pi,
+    ).animate(curvedAnimation) // animController
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          animController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          animController.forward();
+        }
+      });
+
+    animController.forward();
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Transform.rotate(
+        angle: animation.value,
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(90),
+          child: Image.asset('assets/images/czr.png'),
+        ),
+      ),
+    );
+  }
+}
