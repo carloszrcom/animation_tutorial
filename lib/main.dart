389f90c91aefc2245d9d25ea34549dbf66cdce19
CarlosZR <carlos.z.r.78@gmail.com>
2021-06-19 19:37:29 +0200

    Simplificando con AnimatedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'basic_animation_page.dart';
+import 'animated_widget_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-/// Prueba de commit 2.
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
